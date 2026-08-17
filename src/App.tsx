af143b739637cab15626c2f57a3301ef4b3e5712
rodrigo <rodrigomurillo666@hotmail.com>
2026-08-18 01:56:27 +0200

    Added hook for iframe auto-height fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { CartaDigital } from "./components/carta/CartaDigital";
+import { useIframeAutoHeight } from "./hooks/useIframeAutoHeight";
 
 export default function App() {
+  useIframeAutoHeight();
+
   return (
       <main className={"bg-bg text-text min-h-screen scroll-smooth"}>
         <CartaDigital />
